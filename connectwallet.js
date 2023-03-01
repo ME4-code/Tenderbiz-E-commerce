50e9638d1dc1d370b96b4c8dc8578cf934c0461c
Halimat Shaibu <102097588+DrTamTam@users.noreply.github.com>
2023-03-01 20:51:25 +0100

    Update connectwallet.js

--- connectwallet.js
+++ connectwallet.js
@@ -12,19 +12,4 @@ connectButton.addEventListener('click', async () => {
   } else {
     alert('Metamask is not installed! Please install Metamask wallet');
   }
-
-const connectButton = document.getElementById('connect');
-connectButton.addEventListener('click', async () => {
-  if (typeof window.ethereum !== 'undefined') {
-    alert('Metamask is installed!');
-
-    // Connect to Metamask
-    await ethereum.request({ method: 'eth_requestAccounts' });
-    alert('Connected to Metamask!');
-
-    // Redirect to the display page
-    window.location.href = 'connected.html';
-  } else {
-    alert('Metamask is not installed!');
-  }
 });
