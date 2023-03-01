fff182e8bbff2f7515db522fcec3372e89c3bb9a
DrTamTam <102097588+DrTamTam@users.noreply.github.com>
2023-03-01 09:22:22 +0100

    Added javascript codes to connect metamask wallet

---
+++ connectwallet.js
@@ -0,0 +1,15 @@
+const connectButton = document.getElementById('connect');
+connectButton.addEventListener('click', async () => {
+  if (typeof window.ethereum !== 'undefined') {
+    console.log('Metamask is installed!');
+
+    // Connect to Metamask
+    await ethereum.request({ method: 'eth_requestAccounts' });
+    console.log('Connected to Metamask!');
+
+    // Redirect to the display page
+    window.location.href = 'connected.html';
+  } else {
+    console.log('Metamask is not installed!');
+  }
+});
